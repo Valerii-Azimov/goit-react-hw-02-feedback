55a7c5c0d9189135e213ffc5966eb29de94e3bcd
unknown <azimov3.9.1991@gmail.com>
2020-10-09 11:02:50 +0300

    Updates

---
+++ precache-manifest.b918c463b3abfed89e206b844b3a28dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7baff4a054d03e221a89dde097c4a5",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "239859f7673be727c591",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c58d9634.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c58d9634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e76abf0ec5223e70a7",
+    "url": "/goit-react-hw-02-feedback/static/js/main.1b17ae8a.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
